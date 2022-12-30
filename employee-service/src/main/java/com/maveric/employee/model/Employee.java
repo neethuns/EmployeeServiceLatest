3b804254354cf3a6e478c9d8013c38385fd140f6
neethuns <94687937+neethuns@users.noreply.github.com>
2022-12-30 16:46:20 +0530

    Update Employee.java

---
+++ employee-service/src/main/java/com/maveric/employee/model/Employee.java
@@ -0,0 +1,29 @@
+package com.maveric.employee.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+
+    private Integer id;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String address;
+    private String phoneNumber;
+    private String email;
+
+
+
+}
