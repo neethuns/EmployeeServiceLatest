1799ff245fe7a564ebf19345c15489b961f0b671
neethuns <94687937+neethuns@users.noreply.github.com>
2022-12-30 16:37:34 +0530

    Update EmployeeProcessor.java

---
+++ employee-service/src/main/java/com/maveric/employee/config/EmployeeProcessor.java
@@ -0,0 +1,14 @@
+package com.maveric.employee.config;
+
+import com.maveric.employee.model.Employee;
+import org.springframework.batch.item.ItemProcessor;
+
+
+public class EmployeeProcessor implements ItemProcessor<Employee, Employee> {
+
+    @Override
+    public Employee process(Employee employee) {
+        return employee;
+    }
+
+}
